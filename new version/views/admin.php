89065bf67518b02db2aa6137bbdb121871539fa2
Catherine <43898150+CatherineChernogor@users.noreply.github.com>
2020-06-19 01:42:57 +0800

    add search by date

--- new version/views/admin.php
+++ new version/views/admin.php
@@ -4,13 +4,23 @@
 
     <h1>My schedule</h1>
 
+
     <div class="row">
         <div class="col-md-12" v-cloak v-if="!order">
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-outline-success" v-on:click="overdue()">Overdue</button>
-                <button type="button" class="btn btn-outline-success" v-on:click="current()">Current</button>
-                <button type="button" class="btn btn-outline-success" v-on:click="done()">Done</button>
-                <button type="button" class="btn btn-outline-success" v-on:click="all()">All</button>
+            <div class="form-inline">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button type="button" class="btn btn-outline-success" v-on:click="overdue()">Overdue</button>
+                    <button type="button" class="btn btn-outline-success" v-on:click="current()">Current</button>
+                    <button type="button" class="btn btn-outline-success" v-on:click="done()">Done</button>
+                    <button type="button" class="btn btn-outline-success" v-on:click="all()">All</button>
+
+                </div>
+
+                <div class="form-group mx-sm-3 mb-2">
+                    <input type="date" id="searchByDateInput" class="form-control">
+                </div>
+                <button type="button" class="btn btn-success mb-2" v-on:click="serchByDate()">Serch</button>
+
 
             </div>
             <table class="table table-bordered table-hover">
@@ -185,6 +195,37 @@
                     console.log(error);
                     alert("Error, check console");
                 })
+            },
+            serchByDate: function() {
+
+
+                axios.get('').then(response => {
+                    let input = document.getElementById('searchByDateInput');
+                    chosen_date = new Date(input.value);
+                    chosen = {
+                        year: chosen_date.getFullYear(),
+                        month: chosen_date.getMonth(),
+                        day: chosen_date.getDate(),
+                    }
+
+                    let orders = response.data.orders.filter(function(line) {
+                        let new_date = new Date(line.date_end);
+                        let _new = {
+                            year: new_date.getFullYear(),
+                            month: new_date.getMonth(),
+                            day: new_date.getDate(),
+                        }
+                        if (chosen.year === _new.year && chosen.month === _new.month && chosen.day === _new.day)
+                            return line;
+                    });
+
+                    this.orders = orders;
+
+                }).catch(error => {
+                    console.log(error);
+                    alert("Error, check console");
+                })
+
             }
 
         }
